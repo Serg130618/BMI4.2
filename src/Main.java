7e42927eca4326b15c87fac13f57bbbfd12efd0d
ss13 <ssa0618@yandex.ru>
2023-02-05 11:51:57 +0300

    Edit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 82;
-        int height = 170;
+        int weight = 68;
+        int height = 168;
         double Bmiservice = service.calculate(weight, height);
         System.out.println(Bmiservice);
     }
